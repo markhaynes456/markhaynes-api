cf7357cb9f70a92e30fb4e208bd0c6ae7d4b1906
Mark Haynes <markhaynes456@gmail.com>
2015-08-31 20:32:12 +0100

    Updated Projects Controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
 
+  before_filter :set_headers
+  skip_before_filter :verify_authenticity_token
+
   def index
     render json: Project.all
   end
@@ -14,4 +17,11 @@ class ProjectsController < ApplicationController
     Project.find(params[:id])
   end
 
+ def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Expose-Headers'] = 'ETag'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+    headers['Access-Control-Max-Age'] = '86400'
+ end
 end
